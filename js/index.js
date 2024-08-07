f383998e810a5459cae120fcb4648e8240da6e52
Evbgsl <evbgsl@gmail.com>
2024-08-07 19:21:43 +0300

    Code redactored. String functions added.

--- js/index.js
+++ js/index.js
@@ -1,5 +1,16 @@
 import { toCapitalize } from './utils.js';
-import { formatUserName } from './utils.js';
+import { formatFullNameForDoc } from './utils.js';
+import { isPalindrome } from './utils.js';
+import { toCapitalizeNames } from './utils.js';
+import { toStationList } from './utils.js';
 
 console.log(toCapitalize('строка для проверки'));
-console.log(formatUserName('стёПа', 'Семёнов'));
+console.log(formatFullNameForDoc('стёПан ИвАнович СемёНов'));
+console.log(isPalindrome('testtset1'));
+console.log(toCapitalizeNames(['лонДон', 'МанЧЕСТёр', 'БиРминГЕМ', 'лиВЕРпуЛЬ']));
+console.log(toStationList([
+  'MAN675847583748sjt567654;Manchester Piccadilly',
+  'GNF576746573fhdg4737dh4;Greenfield',
+  'LIV5hg65hd737456236dch46dg4;Liverpool Lime Street',
+  'SYB4f65hf75f736463;Stalybridge',
+  'HUD5767ghtyfyr4536dh45dg45dg3;Huddersfield']));
--- js/utils.js
+++ js/utils.js
@@ -9,18 +9,47 @@ export const toCapitalize = (str) => str
   ? `${str[0].toUpperCase()}${str.slice(1)}`
   : '';
 
-  /**
- * @function formatUserName
- * @param {string} firstName
- * @param {string} lastName
- * @returns {string}
+/**
+ * @function formatFullNameForDoc
+ * @param {string} fullName стёПан ИвАнович СемёНов
+ * @returns {string} Степан Иванович Семенов
  */
 
-export const formatUserName = (firstName, lastName) => {
-  const formatName = (name) => {
-    const formattedName = name.toLowerCase().replace(/ё/g, 'е');
-    return formattedName.charAt(0).toUpperCase() + formattedName.slice(1);
-  };
-  return `${formatName(firstName)} ${formatName(lastName)}`;
-}
+export const formatFullNameForDoc = (fullName) => {
+  const str = fullName.toLowerCase().split('ё').join('е');
+  return str.split(' ').map((word) => {
+    return `${word[0].toUpperCase()}${word.slice(1)}`;
+  }).join(' ');
+};
 
+/**
+ * @function isPalindrome
+ * @param {string} str 
+ * @returns {boolean}
+ */
+
+export const isPalindrome = (str) => 
+  str === str.split('').reverse().join('');
+
+/**
+ * @function toCapitalizeNames
+ * @param {string[]} names
+ * @returns {string[]}
+ */
+
+export const toCapitalizeNames = (names) => 
+  names.map((word) => `${word[0].toUpperCase()}${word.slice(1)}`);
+
+/**
+ * @function createStationList
+ * @description Concatenates the first 3 characters 
+ * (station abbr) and all characters after ';' (station name) using ': '
+ * @param {string[]} stationListRawData
+ * @returns {string[]}
+ */
+
+export const toStationList = (stationListRawData) => 
+  stationListRawData.map((station) => {
+    const [abbr, name] = station.split(';');
+    return `${abbr.slice(0, 3)}: ${name}`;
+  });
