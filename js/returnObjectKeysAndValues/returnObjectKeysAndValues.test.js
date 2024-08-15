15cfa2bf39e545041ef11d6c747a8733dc39b027
Evbgsl <evbgsl@gmail.com>
2024-08-15 22:06:57 +0300

    MockData separated

--- js/returnObjectKeysAndValues/returnObjectKeysAndValues.test.js
+++ js/returnObjectKeysAndValues/returnObjectKeysAndValues.test.js
@@ -1,19 +1,5 @@
 import { returnObjectKeysAndValues } from './returnObjectKeysAndValues.js';
-
-const testData = {
-  test1: {
-    obj: { a: 1, b: 2, c: 3 },
-    expectedArr: [['a', 'b', 'c'], [1, 2, 3]],
-  },
-  test2: {
-    obj: { a: 'Apple', c: 'Microsoft', b: 'Google' },
-    expectedArr: [['a', 'b', 'c'], ['Apple', 'Google', 'Microsoft']],
-  },
-  test3: {
-    obj: { key1: true, key2: false, key3: undefined },
-    expectedArr: [['key1', 'key2', 'key3'], [true, false, undefined]],
-  },
-};
+import { testData } from './mockData.js';
 
 describe('returnObjectKeysAndValues', () => {
   test('Unit test 1', () => {
